564af84be6d72e531385679c22da253150b39d20
Joe Gelay <joegelay@gmail.com>
2020-01-30 17:42:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !result[name]
+          result[name] = {}
+        end
+
+        if !result[name][key]
+          result[name][key] = []
+        end
+
+        result[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  return result
 end
