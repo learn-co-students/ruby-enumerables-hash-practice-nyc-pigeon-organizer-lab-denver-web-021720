6da998d19b8ece32e095e60e57ba0730feffbc53
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 21:17:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
-def nyc_pigeon_organizer(data)
+require 'pry'
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
+#def nyc_pigeon_organizer(data)
   # write your code here!
+newHash = {}
+color = data[:color]
+a = color.reduce({}) do |memo,(key, value)|
+  i = 0
+#binding.pry
+  while i<color[key].length
+      #binding.pry
+    if newHash[value[i].to_sym]
+      binding.pry
+      newHash[value[i].to_sym].push(key.to_s)
+    else
+      newHash[value[i].to_sym] = [key.to_s]
+    end
+    i=i+1
+  end
 end
+  puts newHash
+
+#end
