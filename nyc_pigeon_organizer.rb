ebf57ef1325c9147ad9a82b4317260c01ca83714
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 21:50:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,24 +20,55 @@ data = {
 
 
 
-#def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(data)
   # write your code here!
 newHash = {}
 color = data[:color]
+gender = data[:gender]
+lives = data[:lives]
 a = color.reduce({}) do |memo,(key, value)|
   i = 0
-#binding.pry
   while i<color[key].length
-      #binding.pry
-    if newHash[value[i].to_sym]
-      binding.pry
-      newHash[value[i].to_sym].push(key.to_s)
+#binding.pry
+    if newHash[value[i]]
+      newHash[value[i]][:color].push(key.to_s)
     else
-      newHash[value[i].to_sym] = [key.to_s]
+    #  binding.pry
+      newHash[value[i]] = {:color =>[key.to_s] }
     end
     i=i+1
   end
 end
-  puts newHash
 
-#end
+b=gender.reduce({})do |memo,(key, value)|
+#binding.pry
+i=0
+ while i<gender[key].length
+    if newHash[value[i]][:gender]
+      newHash[value[i]][:gender].push(key.to_s)
+    else
+    #  binding.pry
+      newHash[value[i]][:gender] = [key.to_s]
+    end
+    i=i+1
+  end
+
+end
+c=lives.reduce({})do |memo,(key, value)|
+#binding.pry
+i=0
+ while i<lives[key].length
+    if newHash[value[i]][:lives]
+      newHash[value[i]][:lives].push(key.to_s)
+    else
+    #  binding.pry
+      newHash[value[i]][:lives] = [key.to_s]
+    end
+    i=i+1
+  end
+
+end
+  return newHash
+
+end
+puts nyc_pigeon_organizer(data)
