6095c5461bb08e57ade6f89abe0478c24cf5671b
JT Lu <alohajt94@gmail.com>
2020-02-06 00:07:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+def nyc_pigeon_organizer (data)
+  pigeon_hash = {}
+
+  data.each do |inner_hash, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        pigeon_hash[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  keys = pigeon_hash.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      keys.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      keys.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      keys.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  return pigeon_hash 
+end 
